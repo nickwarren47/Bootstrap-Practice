8fc1ca408062987471d74b159ad377b6fcd6f011
Nick Warren <nickwarren47@gmail.com>
2022-08-02 12:52:47 -0600

    Added more bootstrap apps and some react apps as well.

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <h1>
+                Hello 
+                <span className="logo" role="img">
+                    💻
+                </span>
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
