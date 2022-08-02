07a47146619a57ce0aa60fbfb074dd1fd6dfc894
Nick Warren <nickwarren47@gmail.com>
2022-08-01 19:58:43 -0600

    Added basic files to practice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ItemPage from "./ItemPage";
+import Header from "./Header";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <br />
+      <ItemPage />
+    </div>
+  );
+}
+
+export default App;
